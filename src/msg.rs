d6e34d7ac0fd7f8a1b8cbda6e96e463a101e368f
ducphamle2 <ducphamle212@gmail.com>
2021-12-07 10:55:58 +0700

    update scaffold

--- src/msg.rs
+++ src/msg.rs
@@ -1,4 +1,3 @@
-use cosmwasm_std::{Binary, HumanAddr};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -7,17 +6,8 @@ pub struct InitMsg {}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub struct Data {
-    pub name: String,
-    pub prices: Vec<String>,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
 pub struct Response {
-    pub name: String,
-    pub result: Binary,
-    pub status: String,
+    pub data: String,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
@@ -28,16 +18,5 @@ pub enum HandleMsg {}
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum QueryMsg {
-    Test {
-        contract: HumanAddr,
-        input: String,
-        output: String,
-    },
-}
-
-// for query other contract
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-#[serde(rename_all = "snake_case")]
-pub enum DataSourceQueryMsg {
-    Get { input: String },
+    Test { input: String },
 }
